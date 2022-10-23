0e0e2bd2b7676d2855ae6e529d4b771360aebbac
ohad.r <oredlich@mobileye.com>
2022-10-23 17:07:17 +0300

    use an additional appender for writing logs as json

--- src/log-config.ts
+++ src/log-config.ts
@@ -1,5 +1,12 @@
 import * as correlator from 'correlation-id';
-import { configure, getLogger } from "log4js";
+import { configure, getLogger, addLayout } from "log4js";
+
+addLayout("json", function (config) {
+    return function (logEvent) {
+        logEvent["sessionId"] = correlator.getId();
+        return JSON.stringify(logEvent);
+    };
+});
 
 configure({
     appenders: {
@@ -11,9 +18,20 @@ configure({
                         return correlator.getId();
                     }
                 }
+            }},
+        out2: { type: 'stdout', layout: {
+                type: 'json',
+                separator: ",",
+                //pattern: '{%[[%d] [%p] %c [%x{user}]%] %m}',
+                tokens: {
+                    user: function (logEvent) {
+                        return correlator.getId();
+                    }
+                }
             }}
     },
-    categories: { default: { appenders: ['out'], level: 'info' } }
+    categories: { default: { appenders: ['out', 'out2'], level: 'info' }
+        }
 });
 
-getLogger('log-config').info('logger is configured.');
+getLogger('log-config').info({m: 'logger is configured.'});
